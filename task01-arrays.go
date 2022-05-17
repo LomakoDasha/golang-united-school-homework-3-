ad1b8882bf321412be100962d976c77dcf68294b
Darya Lamaka <lomakodasha@gmail.com>
2022-05-18 00:46:30 +0200

    feat: implement task01-array

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,12 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	var arrLength = len(input)
+	var arrSum float32 = 0
+
+	for i := 0; i <= arrLength-1; i++ {
+		arrSum += input[i]
+	}
+
+	return arrSum / float32(arrLength)
 }
