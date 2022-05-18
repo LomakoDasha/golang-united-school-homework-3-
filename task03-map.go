1e8ec43b4b28f6c702ab0c18da6d1785ff4b667d
Darya Lamaka <lomakodasha@gmail.com>
2022-05-18 23:02:07 +0200

    feat: implement task03-map

--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,20 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
-	//Place your code here
-	return
+	var sliceOfKeys []int
+	var sliceOfSortedValues []string
+
+	for key, _ := range input {
+		sliceOfKeys = append(sliceOfKeys, key)
+	}
+
+	sort.Ints(sliceOfKeys)
+
+	for i := 0; i <= len(sliceOfKeys)-1; i++ {
+		sliceOfSortedValues = append(sliceOfSortedValues, input[i])
+	}
+
+	return sliceOfSortedValues
 }
